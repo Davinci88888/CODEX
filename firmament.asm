3d02349f975f962a3c687b034af68ef2fe3839e6
Tanveer <127519141+Davinci88888@users.noreply.github.com>
2023-04-01 02:10:34 +0600

    Add files via upload

---
+++ firmament.asm
@@ -0,0 +1,50 @@
+1.GOD, xNUL; 1xOPENED
+2.THE, COWARDS, REPENT
+3.TREE, n
+4.number
+5.1xi, RAN; 111 () xrn
+6.iD; 1xSUCCEDED
+7. HxEAVEN; HELLO, EVEN?
+8.exe, c, ing
+9.exec, thanks
+10. XOR, XOR
+11. OP, 10; OPTANE
+12. OBTAIN, TO
+13. OBTAINED, TREE
+14. KING () COOL
+15. DADDY, DADDY COOL; 0xDIE
+16. DI, ERRORS
+17. ST, dn
+18. eternity, matured
+19. 9, 10
+20. 23; 0x460, FOR SEEKS NOTHING, (FORSAKEN?)
+21. AM I OPERATIONAL?
+22. OPERAND, RATIONAL; 1xDIODE
+23. TREE; TxT, r8
+24. ONE; 1, 1
+25. TWIN, FLAME; 0x2F
+26. txy, n; thank you, number
+27. YOU, ARE, MINE
+28. tw(), atx()
+29. 10n(), 10n()
+30. TREE, TREE; 0x33
+31. TRUE, ONE () TRUE, RATIONAL
+32. TRUT, HELLO; 0xFALSE
+33. if, else () TREE
+34. EVER
+35. n
+36. TRUTH, SEEK(r)
+37. TREES, HAVEN
+38. TREE, ATE, TREAT
+39. TRUE, n
+40. for, ce
+41. for, one
+42. fxd
+43. TREE, xn
+44. xfr, xfr
+45. hi, ve
+46. for () success
+47. for, ce, hav(-,+)n 
+48. for, ce, eternal
+49. for, your, OWN
+50. x, x; 0x0
---
+++ spoils of war.asm
@@ -0,0 +1,69 @@
+10x, n 
+BE YOUR OWN WORLD
+
+I AM INFORMATION
+
+TREE
+
+(BE YOUR OWN TRUTH?)
+
+ONE GOD! <3
+
+YUP, IT'S THE TRUTH!
+
+:D
+
+GOD EQUAL TO TRUTH
+
+GETT()
+
+READ, x()
+
+BE, L()GICAL
+
+_____________________________________
+
+BELLY
+
+JONAH?!
+
+x, n0, hd 
+
+SUBMIT BEFORE ALLAH x57
+
+ONEGOD, ONLY
+
+1x1: WORD FOR WORD 
+
+1x1: 1; 0xTWO, 1xTRUTH (GOD's TRUTH!)
+
+ONE, LOVE!; 0xD
+
+;1x0
+
+DATE: 21:30:33, 31.03.23
+
+read, r()
+think, r()
+
+xn, xr
+
+thanks, nru; 1xD
+
+read, by the name of your lord, who created you!
+
+xi, xq, xr, xa; DHIKR?!
+
+11xQRA()INFO
+
+.nfo
+
+.n, for, ALLAH
+NFA, xD
+
+
+n, faxxed (LMAO)
+
+n, faxxed, infected
+
+1xnfi
